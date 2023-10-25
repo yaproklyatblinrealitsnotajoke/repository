c1cedf64dce0c7068ac636ee8ec78069e0eed6fa
yaproklyatblinrealitsnotajoke <145468224+yaproklyatblinrealitsnotajoke@users.noreply.github.com>
2023-10-25 17:33:57 +0300

    Update www.sh

--- www.sh
+++ www.sh
@@ -7,26 +7,26 @@ echo "<h1>Ugadai chislo</h1>" > public_html/index.html
 numm=$((RANDOM % 100 + 1))
 for ((i=1; i<=100; i++))
 do
-        echo "<a href=\"$i.html\">$i</a>" >> public_html/index.html
+        echo "<a href=\"$i.html\">$i</a>" >> public_html/index.html
 
-        if [ $i -eq $numm ]
-                then
-                        echo "<h1>Vi ugadali!!!</h1>" >> public_html/$i.html
-                else
-                        if [ $i -gt $numm ]
-                        then
-                                echo "<h1>chislo men'she</h1>" >> public_html/$i.html
-                        else
-                                echo "<h1>cislo bolshe</h1>" >> public_html/$i.html
-                fi
-        fi
+        if [ $i -eq $numm ]
+                then
+                        echo "<h1>Vi ugadali</h1>" >> public_html/$i.html
+                else
+                        if [ $i -gt $numm ]
+                        then
+                                echo "<h1>chislo bolshe</h1>" >> public_html/$i.html
+                        else
+                                echo "<h1>chislo menshe</h1>" >> public_html/$i.html
+                fi
+        fi
 
-        echo "<br>" >> public_html/$i.html
+        echo "<br>" >> public_html/$i.html
 
-        for ((j=1; j<=100; j++))
-        do
-                echo "<a href=\"$j.html\">$j</a>" >> public_html/$i.html
-        done
+        for ((j=1; j<=100; j++))
+        do
+                echo "<a href=\"$j.html\">$j</a>" >> public_html/$i.html
+        done
 
-        echo "<br><br><a href=\"index.html\">home</a>" >> public_html/$i.html
+        echo "<br><br><a href=\"index.html\">home</a>" >> public_html/$i.html
 done
